6eb961311a8d9ea5e00fba6b5510cc136a15ff47
ArtixBot <williamwongcastle@gmail.com>
2018-07-28 22:26:20 -0400

    x and y-coordinates SHOULD be figured out by now christ

--- Peerless/Assets/Scripts/Generation/BoardGenerator.cs
+++ Peerless/Assets/Scripts/Generation/BoardGenerator.cs
@@ -19,9 +19,7 @@ public class BoardGenerator : MonoBehaviour {
 	void Start () {
 		boardHolder = new GameObject("BoardHolder");
 		setUpBoard(rows, rowLength);
-        createRooms();
-
-        initalizeRoomsInBoard();
+        setupRooms();
 	}
 
     // Sets up gameplay board.
@@ -38,13 +36,13 @@ public class BoardGenerator : MonoBehaviour {
      */
 	void setUpBoard (int rows, int rowLength)
     {
-        // Establish a array of ${rows} arrays overall.
+        // Establish a array of {rows} arrays overall.
         tiles = new Tile[rows][];
-        // Go through all the tile arrays...
+        // Go through each row...
         for (int y = 0; y < tiles.Length; y++)
         {
-            // ... and set each tile array is the correct height.
-            // We now have an array of ${rows} arrays, with each of those arrays of length ${rowLength}
+			// ... and set each row to equal {rowLength}.
+            // We now have an array of {rows} arrays, with each of those arrays of length {rowLength}.
             tiles[y] = new Tile[rowLength];
             for (int x = 0; x < tiles[y].Length; x++){
                 tiles[y][x] = new Tile(y, x, "#");
@@ -52,8 +50,8 @@ public class BoardGenerator : MonoBehaviour {
         }
     }
 
-    // Generates rooms.
-    void createRooms(){
+    // Generates rooms and places them on the board.
+    void setupRooms(){
         // Determine the number of rooms to spawn.
         roomList = new Room[numRooms.random];
         Debug.Log("Creating " + roomList.Length + " rooms.");
@@ -64,24 +62,26 @@ public class BoardGenerator : MonoBehaviour {
         roomList[0] = initRoom;
         initRoom.setupStartingRoom(roomWidth, roomHeight, rowLength, rows);
 
-        Debug.Log("Starting room built at row " + initRoom.xPos + ", column " + initRoom.yPos + ", with a width of " + initRoom.roomWidth + " and height of " + initRoom.roomHeight);
+        Debug.Log("Starting room built at column " + initRoom.xPos + ", row " + initRoom.yPos + ", with a width of " + initRoom.roomWidth + " and height of " + initRoom.roomHeight);
+		initalizeRoom (initRoom);
 
         // After first room is generated, generate all future rooms.
         for (int i = 1; i < roomList.Length; i++){
             Room r = new Room();
             roomList[i] = r;
-            //r.setupRoom(roomWidth, roomHeight, rowLength, rows);
+            r.setupRoom(roomWidth, roomHeight, rowLength, rows);
+			initalizeRoom (r);
         }
     }
 
-    // Instantiates rooms so that they appear on the board.
-    void initalizeRoomsInBoard(){
-        foreach (Room room in roomList){
-            //Debug.Log("Initalizing room. (x, y) = (" + room.xPos + "," + room.yPos +"), (w, h) = (" + room.roomWidth + ", " + room.roomHeight +").");
-            for (int row = room.xPos; row < room.xPos + room.roomWidth; row++){
-                for (int rowIndex = room.yPos; rowIndex < room.yPos + room.roomHeight; rowIndex++){
-                    tiles[row][rowIndex].test = ".";
-                }
+    // Called by setupRooms.
+	// Actually makes the room visible in the gameboard.
+    void initalizeRoom(Room r){
+		// row determines y-position (the 'column') while the rowIndex determines x-position (the 'row').
+		// (Possibly?) Unintuitive. I should fix this eventually.
+        for (int row = r.yPos; row < r.yPos + r.roomHeight; row++){
+            for (int rowIndex = r.xPos; rowIndex < r.xPos + r.roomWidth; rowIndex++){
+                tiles[row][rowIndex].test = ".";
             }
         }
     }
